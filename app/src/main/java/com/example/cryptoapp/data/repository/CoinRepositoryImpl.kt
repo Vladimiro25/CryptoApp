82c406e7dce31ceafbba0e12e5d2d4c63bd65750
Vladimiro25 <uzunguk2000@gmail.com>
2025-09-25 15:21:14 +0300

    Change MediatorLiveData to map

--- app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.cryptoapp.data.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.map
 import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiFactory
@@ -11,28 +11,27 @@ import com.example.cryptoapp.domain.CoinRepository
 import kotlinx.coroutines.delay
 
 class CoinRepositoryImpl(
-    application: Application
+    private val application: Application
 ) : CoinRepository {
 
     private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
     private val apiService = ApiFactory.apiService
 
     private val mapper = CoinMapper()
+
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
-        return MediatorLiveData<List<CoinInfo>>().apply {
-            addSource(coinInfoDao.getPriceList()) {
-                it.map { coinInfoDbModel ->
-                    mapper.mapDbModelToEntity(coinInfoDbModel)
-                }
+        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceList().map
+        return coinInfoDao.getPriceList().map {
+            it.map {
+                mapper.mapDbModelToEntity(it)
             }
         }
     }
 
     override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
-        return MediatorLiveData<CoinInfo>().apply {
-            addSource(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
-                mapper.mapDbModelToEntity(it)
-            }
+        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map
+        return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
+            mapper.mapDbModelToEntity(it)
         }
     }
 
@@ -40,12 +39,12 @@ class CoinRepositoryImpl(
         while (true) {
             try {
                 val topCoins = apiService.getTopCoinsInfo(limit = 50)
-                val fromSymbols = mapper.mapNamesListToString(topCoins)
-                val jsonContainer = apiService.getFullPriceList(fSyms = fromSymbols)
+                val fSyms = mapper.mapNamesListToString(topCoins)
+                val jsonContainer = apiService.getFullPriceList(fSyms = fSyms)
                 val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
                 val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
                 coinInfoDao.insertPriceList(dbModelList)
-            } catch (_: Exception) {
+            } catch (e: Exception) {
             }
             delay(10000)
         }
